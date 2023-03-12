0affbe6bc1e2087e0107569db25293207014e35e
Aleksandr Grigoryev <AleksandrGrigoryev@itcenterifmo.onmicrosoft.com>
2023-03-12 18:24:18 +0300

    Добавьте файлы проекта.

---
+++ ITMO.ASP.NET_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.ASP.NET_MVC.MvcApplication" Language="C#" %>
---
+++ ITMO.ASP.NET_MVC/Models/PersonRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET_MVC.Models
+{
+    public class PersonRepository
+    {
+        private List<Person> persons = new List<Person>();
+        public int NumberOfPerson
+        {
+            get
+            {
+                return persons.Count();
+            }
+        }
+        public IEnumerable<Person> GetAllResponses
+        {
+            get
+            {
+                return persons;
+            }
+        }
+        public void AddResponse(Person pers)
+        {
+            persons.Add(pers);
+        }
+
+    }
+}
